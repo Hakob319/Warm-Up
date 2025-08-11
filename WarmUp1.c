251aa4ca5e2717a803dbded58ae4905543c898bf
Hakob319 <hakobharutyunyan014@gmail.com>
2025-08-11 17:12:45 +0400

    Create WarmUp1.c

---
+++ WarmUp1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+        int arr1[3] = {2, 5, 7};
+        int arr2[3] = {1, 4, 8};
+        int sum;
+
+
+        for (int i = 0; i < 3; ++i) {
+
+        sum = arr1[i] * arr2[i];
+
+printf("%d\n", sum);
+
+        }
+return 0;
+
+}
